c3b6ef7a4c70c72815ab8d6647d1f0c847a48db2
Eduardo Tellez <ippalin@gmail.com>
2015-11-02 14:48:02 -0800

    fixed things up

--- src/MapReset/Main.php
+++ src/MapReset/Main.php
@@ -2,6 +2,7 @@
 
 namespace MapReset;
 
+use pocketmine\Server;//$this->getServer() come from this
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use MapReset\Perms;
@@ -10,6 +11,7 @@ use pocketmine\event\Listener;
 class Main extends PluginBase  implements Listener {
  
 public $Command;
+public $map;
  
  public function onLoad() { 
    // $this->getLogger()->info(TextFormat::RED . "Loading MapReset"); They would see this message twice if you use onLoad & onEnable.
@@ -19,8 +21,8 @@ public $Command;
         @mkdir($this->getDataFolder());
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
-        $map = $this->getConfig();
-        $time = $map->get("TimeReset"); //Grabs time until reset
+        $this->map = $this->getConfig();
+        $time = intval($this->map->get("TimeReset")) * 20; //Grabs time until reset
         $this->getConfig()->set("startreset", "no"); //This is to make sure there wont be a map reset on onEnable
         $this->getServer()->getScheduler()->scheduleRepeatingTask(new MapResetCountDown($this), $time); //sechdules a task for the time.
         $this->startReset(); //Start reset timer function.
@@ -48,12 +50,15 @@ public $Command;
         }
         $this->getLogger()->info(TextFormat::GREEN . "[MapReset] MapReset version 1.0.0 has been enabled.");
     }
+ /*
+//You already created schedule repeating task on enable!
  public function startReset(){
   $map = $this->getConfig();
   $timer = $map->get("TimeReset");
-  $time = intval($timer * 20); //Fixes things up :D
-  $this->getServer()->getScheduler()->scheduleDelayedTask(new CallbackTask([$this, "resetNow"], [$sender]), $time);
+  $time = intval($timer) * 20; //Fixes things up :D
+  $this->getServer()->getScheduler()->scheduleDelayedTask(new CallbackTask([$this, "resetNow"], [$sender]/*Undefined var $sender*/), $time);
  }
+ */
  public function resetNow(){
   $this->getLogger()->info("[MapReset] Resetting world. Please wait.");
  }
